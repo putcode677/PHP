d77f6753ade9e672357a6090df812a45b666ed80
Pascal <pascalmtavangu95@gmail.com>
2026-08-30 23:51:55 +0300

    i added an square operator calculatio

--- test.php
+++ test.php
@@ -4,12 +4,17 @@
 $x = 20;
 $y = 56;
 
-$diff = $x - $y;
+$diff = $y - $x;
 $sum  = $x + $y;
 $mod=$x%3;
+$square1=$x**2;
+$square2=$y**2;
 
-echo "the difference is : " . $diff . "<br>";
-echo "the sum is : " . $sum;
-echo "the module is: " .$mod;
+echo "the difference is :$diff<br>";
+echo "the sum is :$sum<br>";
+echo "the module is:$mod<br>";
+echo "the square1 is : $square1<br>";
+
+
 
 ?>
