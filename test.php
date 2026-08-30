94905e7eef20c482e0eb4d2adb39b0e637e30d76
Pascal <pascalmtavangu95@gmail.com>
2026-08-30 23:39:53 +0300

    i added a file for my learning (test.php)

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+//arithmetic operators
+
+$x = 20;
+$y = 56;
+
+$diff = $x - $y;
+$sum  = $x + $y;
+$mod=$x%3;
+
+echo "the difference is : " . $diff . "<br>";
+echo "the sum is : " . $sum;
+echo "the module is: " .$mod;
+
+?>
